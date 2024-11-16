04709b80de9a7f6ac6e78d707e3e2087f3f5c2d0
Hemanth5603 <shemanth2003.vskp@gmail.com>
2024-11-16 23:49:33 +0530

    manages routes

--- customer_service/urls.py
+++ customer_service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('submit/', views.submit_request, name='submit_request'),
     path('track/', views.track_requests, name='track_requests'),
     path('manage/', views.manage_requests, name='manage_requests'),
     path('register/', views.register, name='register'),
-    path('login/', views.custom_login, name='login'),
-]
+    path('login/', auth_views.LoginView.as_view(template_name='customer_service/login.html'), name='login'),
+]   
